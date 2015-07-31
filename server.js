6690419da8369874bec5deefa9105a5c5cc282d0
hali <hali@tinyrx.co>
2015-07-31 15:59:41 -0700

    set up app for heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 sslmode=require;
 
 var pg = require('pg');
+var app = require('express')();
 
 var client = new pg.Client({
   user: "xgqflynipwwdac",
@@ -15,13 +16,27 @@ client.connect(function(err) {
 	if(err) {
 		return console.error('could not connect to postgres', err);
 	}
-	client.query('SELECT COUNT(*) FROM "Signups"', function(err, result) {
-		if(err) {
-			return console.error('error running query', err);
-		}
-		var count = result.rows[0].count;
-		console.log(count);
-		client.end();
-	});
+
+	app.set('port', (process.env.PORT || 5000));
+    app.get('/', function(req, res) {
+  		client.query('SELECT COUNT(*) FROM "Signups"', function(err, result) {
+			if(err) {
+				return console.error('error running query', err);
+			}
+			var count = result.rows[0].count;
+			console.log(count);
+			var payload = 
+          	{
+            	"item": [
+              		{ "text": "Completed Signups", value: count},
+            	]
+          	};
+        	res.send(JSON.stringify(payload));
+			client.end();
+		});
+    });
+	app.listen(app.get('port'), function() {
+        console.log("running at localhost:" + app.get('port'))
+    });
 });
 
